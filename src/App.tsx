c7231cbbb2244651a95a44079326984c8a763967
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-02-15 15:03:30 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import ComponentNavbar from "./components/ComponentNavbar";
+
+function App() {
+  return (
+    <>
+      <header>
+        <ComponentNavbar />
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
